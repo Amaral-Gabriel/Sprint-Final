f64fe010dfa6c6ade0c7a9f787540d4cf2effd64
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2025-05-20 22:57:15 -0300

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -41,10 +41,10 @@ export default {
     'index.server.html': {size: 1142, hash: '83f9d57f82f876e0595e9a634705a1f19f50bd6f6caa997ae1ffd5034a963cd6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'home-site/index.html': {size: 39455, hash: '2393fe984db11e6413ef47f41e5c9b28ffebe2f0dcdc32b2137d0c16beb5c48e', text: () => import('./assets-chunks/home-site_index_html.mjs').then(m => m.default)},
     'login/index.html': {size: 38933, hash: 'a60a0c2bc792c764ffc3ea4594b75189d9e445ee0c48ff412da3b48821c4d751', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 39983, hash: 'be2d5c9b6ce66178584fa975f356e0e9f91c176a718610453e227da8c219a762', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'lancamentos/index.html': {size: 49705, hash: '0a21c861bc7954adf10a8b5b065add20c1600fb6de740e1c8de7dedb4fb513ef', text: () => import('./assets-chunks/lancamentos_index_html.mjs').then(m => m.default)},
     'contato/index.html': {size: 54304, hash: '610342c8dd339aec33a67549939b5d653a40ee4867f6f111bb891997df153ae0', text: () => import('./assets-chunks/contato_index_html.mjs').then(m => m.default)},
-    'dashboard/index.html': {size: 47635, hash: 'cb145ecdd48431250ba5d01939eb3e9a58555dea531179397a9538d49cbf84d5', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 39983, hash: 'be2d5c9b6ce66178584fa975f356e0e9f91c176a718610453e227da8c219a762', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'dashboard/index.html': {size: 47635, hash: '4b1c5dcc39b999ed72a22db94f9cb10a5ab2357baad85ea99f0f9c5bc4fe11f7', text: () => import('./assets-chunks/dashboard_index_html.mjs').then(m => m.default)},
     'styles-EHUMQJKX.css': {size: 250753, hash: 'jOWDZBz5GwI', text: () => import('./assets-chunks/styles-EHUMQJKX_css.mjs').then(m => m.default)}
   },
 };
